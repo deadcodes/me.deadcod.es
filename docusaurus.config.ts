1d1f15fe35efa9e36db3e9ef1c71712dfde5fd1b
deadcodes <86150272+deadcodes@users.noreply.github.com>
2024-02-05 10:16:40 +0530

    path name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://me.deadcod.es',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/me.deadcod.es',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
