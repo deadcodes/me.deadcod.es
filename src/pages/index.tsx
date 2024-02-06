dd3547a8091be15a3032cc8929fe78f69e25638a
deadcodes <86150272+deadcodes@users.noreply.github.com>
2024-02-06 23:07:29 +0530

    cleaned up landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,18 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+
 import '../fonts/Monoton-Regular.ttf'
-import {
-  FluentProvider,
-  webDarkTheme,
-} from "@fluentui/react-components";
+
 import  { Redirect } from 'react-router-dom';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (<Redirect to='/category/scripts' />);
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
